453c042c04366538e8946cc67449764e59b495c5
Juan Guilherme <asjuanguilherme@gmail.com>
2022-04-09 16:31:11 -0300

    feat: :sparkles: Endpoint '/teams/:id' para get e post de time

--- index.js
+++ index.js
@@ -41,3 +41,56 @@ app.get("/teams", (req, res) => {
       res.send("Não existem times cadastrados")
     })
 })
+
+app.post("/teams/:id", (req, res) => {
+  const name = req.body.name
+  const characters = req.body.characters
+  const id = parseInt(req.params.id)
+
+  if (!name || !characters || !id)
+    return res.send("Todos os campos são obrigatórios")
+  if (!isJson(characters) || typeof JSON.parse(characters) !== "object")
+    return res.send(
+      "O campo 'characters' deve ser um array com os ids dos pokemons do time."
+    )
+  if (JSON.parse(characters).length < 6)
+    return res.send("Um time deve ter 6 pokemons.")
+
+  PokemonTeam.findByPk(id)
+    .then(queryRes => {
+      if (queryRes === null)
+        return res.send("Não foi possível encontrar o time com o id informado")
+
+      PokemonTeam.update(
+        { name: name, characters: characters },
+        { where: { id: id } }
+      )
+        .then(data => res.send(data))
+        .catch(err => {
+          console.log(err)
+          res.send("Não foi possível editar o time com o id informado")
+        })
+    })
+    .catch(err => {
+      console.log(err)
+      res.send(err)
+    })
+})
+
+app.get("/teams/:id", (req, res) => {
+  const id = parseInt(req.params.id)
+
+  if (typeof id !== "number")
+    res.send("O parametro de busca de um pokemon deve ser o ID")
+
+  PokemonTeam.findByPk(id)
+    .then(queryRes =>
+      queryRes === null
+        ? res.send("Não foi possível encontrar pokemon para o id específicado")
+        : res.send(data)
+    )
+    .catch(err => {
+      res.send(err)
+      console.log(err)
+    })
+})
