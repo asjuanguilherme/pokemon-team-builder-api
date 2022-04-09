c6aa3579332b0bbc2bb375224e361a2d744045cc
Juan Guilherme <asjuanguilherme@gmail.com>
2022-04-09 14:23:56 -0300

    feat: :sparkles: Conexão com o banco de dados
    Implementada conexão com o banco de dados pelo sequelize. Adição de arquivo de variáveis de ambiente (.env). Novas dependências: sequelize, mysql2 e dotenv

---
+++ database/config.js
@@ -0,0 +1,25 @@
+const Sequelize = require("sequelize")
+require("dotenv").config()
+
+const {
+  DATABASE_NAME,
+  DATABASE_USER,
+  DATABASE_PASS,
+  DATABASE_HOST,
+  DATABASE_PORT,
+} = process.env
+
+const connection = new Sequelize(DATABASE_NAME, DATABASE_USER, DATABASE_PASS, {
+  host: DATABASE_HOST,
+  dialect: "mysql",
+  port: DATABASE_PORT,
+})
+
+module.exports = connection
+  .authenticate()
+  .then(() =>
+    console.log(
+      `Conectado ao banco de dados '${DATABASE_NAME}' como '${DATABASE_USER}'.`
+    )
+  )
+  .catch(err => console.error(err))
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express")
+require("./database/config")
 
 const app = express()
 const port = 4000
